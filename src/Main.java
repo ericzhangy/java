a74deb10e4c3fbfb4effd7e4a84d3251fea85f99
eric <166269@qq.com>
2024-02-20 17:45:25 +0800

    Changes

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+
+        FreshJuice juice;
+        juice = new FreshJuice();
+        juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
+
+    }
+}
+
+class FreshJuice{
+    enum FreshJuiceSize{SMALL,MEDIUM,LARGE}
+    FreshJuiceSize size;
+
+    public void test(){
+        int age = 1;
+        System.out.println(age);
+
+    }
+
+}
